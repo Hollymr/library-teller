29dd3295f75f65350d893e93bec8dcacfcb88896
Holly Rhodes <hllyrhodes@gmail.com>
2016-06-30 14:42:19 -0400

    fileIO

--- Library/FileIO.cs
+++ Library/FileIO.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Library
+
 {
     class FileIO : IFileInterface
     {
@@ -19,33 +20,39 @@ namespace Library
                 //check to see whether the file exists
                 if (!File.Exists(path))
                 {
-                    //throw appropriate exepction
-                        //implement
+                    throw new Exception ("File does not exist!");
                 }
 
                 //initialize the empty string we'll be using for each line
                 string line;
 
-                //open a StreamReader to the file specifcied in the path variable 
-                    //implement
+                
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    line = reader.ReadLine();
+                }
+                Console.WriteLine(line);
 
-                //while reading a new from file, addd each line to mediaFile
-                //as long as each new line is not null
-                    //implement
+                int counter = 0;
+                // Read file display line by line
+                System.IO.StreamReader file =
+                new System.IO.StreamReader(@"C:\Users\WeCanCodeIT\Documents\Visual Studio 2015\Projects\library-teller\Library\Media.txt");
 
-
-                //remember to close your file
-                //file.Close();
+                while ((line = file.ReadLine()) !=null)
+                    {
+                        Console.WriteLine(line);
+                        counter++;
+                    }
+                
+                    file.Close();
             }
             catch (FileNotFoundException)
             {
-                //write out appropriate message
-                    //implement
+                Console.WriteLine("File not found!");
             }
             catch (Exception e)
             {
-                //write out the message of e
-                    //implement
+                Console.WriteLine("Unknown error occured.");
             }
 
             return mediaFile;
